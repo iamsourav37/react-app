036e632be4c74264a184244aac9a562584b5f30b
iamsourav37 <souravganguly108@gmail.com>
2021-04-26 17:54:39 +0530

    currentWidth changing using useEffect

--- src/components/UseEffectHooksDemo.jsx
+++ src/components/UseEffectHooksDemo.jsx
@@ -2,17 +2,30 @@ import React, { useEffect, useState } from "react";
 
 function UseEffectHooksDemo() {
   const [count, setCount] = useState(0);
+  const [name, setName] = useState("Sourav");
+  const [withCount, setWithCount] = useState(window.screen.width);
 
   useEffect(() => {
+    console.log("first useEffect");
     if (count !== 0) document.title = `Count (${count})`;
     else document.title = `Count`;
   }, [count]);
 
-  console.log("after useEffect block");
+  useEffect(() => {
+    console.log("Second useEffect");
+  }, [name]);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWithCount(window.innerWidth);
+    });
+  });
 
   return (
     <div className="container text-center mt-5">
-      <h1 className="display-5 text-info mb-5">use effect hook demo</h1>
+      <h1 className="display-5 text-info mb-5 text-uppercase">
+        use effect hook demo. (Current Screen Size : {withCount})
+      </h1>
       <h2 className="display-6">{count}</h2>
       <button
         onClick={() => {
@@ -22,6 +35,21 @@ function UseEffectHooksDemo() {
       >
         +
       </button>
+
+      <div className="container mt-5">
+        <h1 className="display-5 text-info mb-5 text-uppercase">
+          another useEffect
+        </h1>
+        <h2 className="display-6">{name}</h2>
+        <button
+          onClick={() => {
+            setName("Sourav Ganguly");
+          }}
+          className="btn btn-dark"
+        >
+          Change Name
+        </button>
+      </div>
     </div>
   );
 }
