59969923e90eadea843ee9716586272cca67ab00
iamsourav37 <souravganguly108@gmail.com>
2021-04-27 10:13:12 +0530

    useReducer hook demo

--- src/components/UseEffectHooksDemo.jsx
+++ src/components/UseEffectHooksDemo.jsx
@@ -1,15 +1,39 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef, useReducer } from "react";
+
+const reducer = (state, action) => {
+  console.log(action);
+  switch (action.type) {
+    case "Increment":
+      state++;
+      if (state >= 10) {
+        alert("You cross 10 !");
+      }
+      return state;
+
+    case "Decrement":
+      state--;
+      if (state < 0) {
+        alert("You dont go negative");
+        return 0;
+      }
+      return state;
+
+    default:
+      break;
+  }
+};
 
 function UseEffectHooksDemo() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, 0);
   const [name, setName] = useState("Your name here...");
   const [withCount, setWithCount] = useState(window.innerWidth);
 
-  useEffect(() => {
-    console.log("first useEffect");
-    if (count !== 0) document.title = `Count (${count})`;
-    else document.title = `Count`;
-  }, [count]);
+  // useEffect(() => {
+  //   console.log("first useEffect");
+  //   if (count !== 0) document.title = `Count (${count})`;
+  //   else document.title = `Count`;
+  // }, [count]);
 
   useEffect(() => {
     window.addEventListener("resize", () => {
@@ -30,14 +54,22 @@ function UseEffectHooksDemo() {
       <h1 className="display-5 text-info mb-5 text-uppercase">
         use effect hook demo. (Current Screen Size : {withCount})
       </h1>
-      <h2 className="display-6">{count}</h2>
+      <h2 className="display-6">{state}</h2>
       <button
         onClick={() => {
-          setCount(count + 1);
+          dispatch({ type: "Increment" });
         }}
         className="btn btn-dark"
       >
         +
+      </button>
+      <button
+        onClick={() => {
+          dispatch({ type: "Decrement" });
+        }}
+        className="btn btn-dark ms-3"
+      >
+        -
       </button>
 
       <div className="container mt-5">
